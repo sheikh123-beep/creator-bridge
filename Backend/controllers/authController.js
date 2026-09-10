19ef5fa09d5bc9f1a503cabe80e75874801b217d
Sheikh Abubakar <itsabubakar0707@gmail.com>
2026-09-11 00:24:27 +0530

    Fix Gmail SMTP IPv4 connection

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -6,15 +6,21 @@ const User = require("../models/User");
 // =========================
 // EMAIL CONFIGURATION
 // =========================
-
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4,
+
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_APP_PASSWORD,
   },
-});
 
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
+});
 // =========================
 // GENERATE OTP
 // =========================
